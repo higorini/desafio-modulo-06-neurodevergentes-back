defe4427094330bcfbb9bc296772a95cf25b0ae4
Teles23 <92751944+Teles23@users.noreply.github.com>
2023-09-13 23:50:59 -0300

    Atualizei o controlador de registrar usuário para usar o novo esquema de banco de dados e adicionar validação à rota.

--- src/controllers/registerUser.js
+++ src/controllers/registerUser.js
@@ -1,8 +1,11 @@
+const bcrypt = require("bcrypt");
+const knex = require("../database/connection");
+
 const registerUser = async (req, res) => {
 	try {
 		const { name, email, password } = req.body;
 
-		const existingUser = await knex("users").where({ email }).first();
+		const existingUser = await knex("usuarios").where({ email }).first();
 
 		if (existingUser) {
 			return res.status(400).json({ message: "E-mail já cadastrado." });
@@ -10,10 +13,15 @@ const registerUser = async (req, res) => {
 
 		const hashedPassword = await bcrypt.hash(password, 10);
 
-		await knex("users").insert({ name, email, password: hashedPassword });
+		await knex("usuarios").insert({
+			nome: name,
+			email,
+			senha: hashedPassword,
+		});
 
 		res.status(201).json({ message: "Usuário cadastrado com sucesso." });
 	} catch (error) {
+		console.log(error);
 		res.status(500).json({ message: "Ocorreu um erro interno." });
 	}
 };
--- src/middlewares/validateRegister.js
+++ src/middlewares/validateRegister.js
@@ -1,11 +1,17 @@
 const schemaValidRegister = require("../schemas/schemaValidateRegister");
 
 const validate = async (req, res, next) => {
+	const { name, email, password } = req.body;
 	try {
+		if (!name & !email & !password) {
+			return res
+				.status(400)
+				.json({ message: "Todos os campos são obrigatórios" });
+		}
 		await schemaValidRegister.validateAsync(req.body);
 		next();
 	} catch (erro) {
-		res.status(500).json({ message: "Ocorreu um erro interno." });
+		res.status(400).json({ message: erro.message });
 	}
 };
 
