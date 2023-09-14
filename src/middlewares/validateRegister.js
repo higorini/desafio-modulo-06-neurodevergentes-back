13826f9010063970fff71014b289a655d00ef2c2
Thiago <92751944+Teles23@users.noreply.github.com>
2023-09-14 13:29:23 -0300

    Atualizei os esquemas de validação para registro de usuário

--- src/middlewares/validateRegister.js
+++ src/middlewares/validateRegister.js
@@ -1,22 +1,22 @@
 const {
-	schemaValidateEmailPassword,
-	schemaValidateName,
-} = require("../schemas/schemaValidateRegister");
+  schemaValidateEmailPassword,
+  schemaValidateName,
+} = require("../schemas/schemaValidateUser");
 
 const validate = async (req, res, next) => {
-	const { name, email, password } = req.body;
-	try {
-		if (!name & !email & !password) {
-			return res
-				.status(400)
-				.json({ message: "Todos os campos são obrigatórios" });
-		}
-		await schemaValidateEmailPassword.validateAsync(req.body);
-		await schemaValidateName.validateAsync(req.body);
-		next();
-	} catch (erro) {
-		res.status(400).json({ message: erro.message });
-	}
+  const { name, email, password } = req.body;
+  try {
+    if (!name & !email & !password) {
+      return res
+        .status(400)
+        .json({ message: "Todos os campos são obrigatórios" });
+    }
+    await schemaValidateEmailPassword.validateAsync(req.body);
+    await schemaValidateName.validateAsync(req.body);
+    next();
+  } catch (erro) {
+    res.status(400).json({ message: erro.message });
+  }
 };
 
 module.exports = validate;
