ea29e8f01759fa90b0461a46ff1d78c9e29a8d03
Teles23 <92751944+Teles23@users.noreply.github.com>
2023-09-14 22:09:18 -0300

    corrigindo intermediario do cadastro

--- src/middlewares/validateRegister.js
+++ src/middlewares/validateRegister.js
@@ -1,22 +1,23 @@
 const {
-  schemaValidateEmailPassword,
-  schemaValidateName,
+	schemaValidateEmailPassword,
+	schemaValidateName,
 } = require("../schemas/schemaValidateUser");
 
 const validate = async (req, res, next) => {
-  const { name, email, password } = req.body;
-  try {
-    if (!name & !email & !password) {
-      return res
-        .status(400)
-        .json({ message: "Todos os campos são obrigatórios" });
-    }
-    await schemaValidateEmailPassword.validateAsync(req.body);
-    await schemaValidateName.validateAsync(req.body);
-    next();
-  } catch (erro) {
-    res.status(400).json({ message: erro.message });
-  }
+	const { name, email, password } = req.body;
+	try {
+		if (!name & !email & !password) {
+			return res
+				.status(400)
+				.json({ message: "Todos os campos são obrigatórios" });
+		}
+		await schemaValidateEmailPassword.validateAsync({ email, password });
+		await schemaValidateName.validateAsync({ name });
+		next();
+	} catch (erro) {
+		console.log(erro);
+		res.status(400).json({ message: erro.message });
+	}
 };
 
 module.exports = validate;
