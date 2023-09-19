5f9b48e8397ef7b44c1d56b08528de691bcb6299
Teles23 <92751944+Teles23@users.noreply.github.com>
2023-09-18 22:19:46 -0300

    Corrigindo url

--- src/services/apiCorreios.js
+++ src/services/apiCorreios.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
 const api = axios.create({
-	baseURL: " viacep.com.br/ws/",
+	baseURL: "https://viacep.com.br/ws/",
 	timeout: 10000,
 	header: { "Content-Type": "application/json" },
 });
