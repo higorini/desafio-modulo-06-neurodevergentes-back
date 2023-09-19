2cdd40ef32ea5571a76e41ebc0ed9804c57a100f
Teles23 <92751944+Teles23@users.noreply.github.com>
2023-09-19 20:12:42 -0300

    adicionado validação de string pros campos

--- src/schemas/schemaValidateCostumer.js
+++ src/schemas/schemaValidateCostumer.js
@@ -2,15 +2,18 @@ const joi = require("joi");
 
 const schemaValidateCostumer = joi.object({
 	phone: joi.string().required().messages({
+		"string.base": "O telefone deve ser uma string",
 		"any.required": "Telefone é obrigatório",
 		"string.empty": "Telefone é obrigatório",
 		"string.length": "Telefone deve ter 15 caracteres",
 	}),
 	status: joi.string().required().messages({
+		"string.base": "Status do cliente deve ser uma string",
 		"any.required": "Status é obrigatório",
 		"string.empty": "Status é obrigatório",
 	}),
 	cpf: joi.string().length(11).required().messages({
+		"string.base": "O cpf deve ser uma string",
 		"any.required": "CPF é obrigatório",
 		"string.empty": "CPF é obrigatório",
 		"string.length": "CPF deve ter 11 caracteres",
