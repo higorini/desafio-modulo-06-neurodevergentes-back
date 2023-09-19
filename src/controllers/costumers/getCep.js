93954e305ffb7250cefba20009db9be3697278b4
Francisco Walter <franciscowalterd@gmail.com>
2023-09-19 05:19:44 -0300

    validando entrada de dados no cep e retornando dados  para o frontend

--- src/controllers/costumers/getCep.js
+++ src/controllers/costumers/getCep.js
@@ -2,9 +2,23 @@ const api = require("../../services/apiCorreios");
 
 const getCep = async (req, res) => {
 	const { cep } = req.body;
+
+	const cepClean = cep.split('').map((number) =>  {
+		if (!isNaN(number)) return number;
+	});
+
 	try {
-		const response = await api.get(`${cep}/json/`);
-		return res.json(response.data);
+		const response = await api.get(`${cepClean.join('')}/json/`);
+
+		return res
+		.status(200).json({
+			cep: response.data.cep,
+			public_place: response.data.logradouro,
+			complement: response.data.complemento,
+			neighborhood: response.data.bairro,
+			city: response.data.localidade,
+			state: response.data.uf
+		});
 	} catch (error) {
 		return res.status(500).json({ message: error });
 	}
