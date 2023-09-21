6eb1a659af0f27046dd2f23f867cfaa008157727
Francisco Walter <franciscowalterd@gmail.com>
2023-09-21 14:11:54 -0300

    adicionando modificações nos schemas após testar as rotas ja existentes

--- src/schemas/schemaRegisterCostumer.js
+++ src/schemas/schemaRegisterCostumer.js
@@ -4,18 +4,21 @@ const schemaRegisterCostumer = joi.object({
 	name: joi.string().required().messages({
 		"any.required": "Nome é obrigatório",
 		"string.empty": "Nome é obrigatório",
+		"string.base": "O Nome deve ser uma string"
 	}),
 
 	email: joi.string().email().required().messages({
 		"any.required": "E-mail é obrigatório",
 		"string.empty": "E-mail é obrigatório",
-		"string.email": "Por favor, forneça um endereço de e-mail válido.",
+		"string.base": "O Nome deve ser uma string",
+		"string.email": "Por favor, forneça um endereço de e-mail válido."
 	}),
 
 	cpf: joi.string().required().min(11).messages({
 		"any.required": "CPF é obrigatório",
 		"string.empty": "CPF é obrigatório",
-		'string.min': "O CPF deve conter no mínimo 11 dígitos"
+		"string.base": "O CPF deve ser uma string",
+		"string.min": "O CPF deve conter no mínimo 11 dígitos"
 	}),
 
 	phone: joi.string().required().min(11).messages({
