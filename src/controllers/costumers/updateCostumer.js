5c6230d70b9cce9aa10ba1e55d739fce78e808e2
Teles23 <92751944+Teles23@users.noreply.github.com>
2023-09-22 01:14:37 -0300

    separei a validação em arquivo separado

--- src/controllers/costumers/updateCostumer.js
+++ src/controllers/costumers/updateCostumer.js
@@ -1,4 +1,5 @@
 const knex = require("../../database/connection/connection");
+const validateAndUpdateCostumer = require("../../utils/checkForExistingCustomer");
 const capitalizeFullName = require("../../utils/capitalizeName");
 
 const updateCostumer = async (req, res) => {
@@ -36,52 +37,8 @@ const updateCostumer = async (req, res) => {
 
 		return res.json(formattedData);
 	} catch (error) {
-		console.error(error);
 		res.status(500).json({ message: "Ocorreu um erro interno." });
 	}
-};
-
-const validateAndUpdateCostumer = async (costumerId, userId, costumerData) => {
-	const { email, cpf } = costumerData;
-
-	const validationMessages = {};
-
-	const isEmailInvalid = await checkForExistingEmail(email, costumerId);
-	const isCpfInvalid = await checkForExistingCpf(cpf, costumerId);
-
-	if (isEmailInvalid) {
-		validationMessages.email = "O e-mail já está em uso.";
-	}
-
-	if (isCpfInvalid) {
-		validationMessages.cpf = "O CPF já está em uso.";
-	}
-
-	if (!isEmailInvalid && !isCpfInvalid) {
-		await knex("costumers")
-			.where({ id: costumerId, user_id: userId })
-			.update(costumerData);
-	}
-
-	return validationMessages;
-};
-
-const checkForExistingEmail = async (email, costumerId) => {
-	const existingEmail = await knex("costumers")
-		.where({ email })
-		.andWhereNot({ id: costumerId })
-		.first();
-
-	return existingEmail;
-};
-
-const checkForExistingCpf = async (cpf, costumerId) => {
-	const existingCpf = await knex("costumers")
-		.where({ cpf })
-		.andWhereNot({ id: costumerId })
-		.first();
-
-	return existingCpf;
 };
 
 const getCostumerById = async (costumerId) => {
