0de43076a896b0a26e9f9200812e2fbe24b29506
Teles23 <92751944+Teles23@users.noreply.github.com>
2023-09-21 23:53:13 -0300

    removi console.log e coloquei pra retornar apenas o objeto inserido

--- src/controllers/charges/postCharges.js
+++ src/controllers/charges/postCharges.js
@@ -1,36 +1,31 @@
 const knex = require("../../database/connection/connection");
 
 const postCharges = async (req, res) => {
-    const { idCostumer } = req.params;
+	const { idCostumer } = req.params;
 
-    const {
-        costumer_name,
-        description,
-        status,
-        value,
-        charge_date } = req.body;
+	const { costumer_name, description, status, value, charge_date } = req.body;
 
-    try {
+	try {
+		const costumer = await knex("costumers").where("id", idCostumer).first();
 
-        const costumer = await knex("costumers").where("id", idCostumer).first();
+		if (!costumer)
+			return res.status(404).json({ message: "Cliente não econtrado!" });
 
-        if (!costumer) return res.status(404).json({ message: "Cliente não econtrado!" });
+		const newCharge = await knex("charges")
+			.insert({
+				costumer_id: idCostumer,
+				costumer_name,
+				description,
+				status,
+				value,
+				charge_date,
+			})
+			.returning("*");
 
-        const newCharge = await knex("charges").insert({
-            costumer_id: idCostumer,
-            costumer_name,
-            description,
-            status,
-            value,
-            charge_date
-        }).returning("*");
-
-        return res.status(200).json(newCharge);
-
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({ message: "Ocorreu um erro interno." });
-    }
+		return res.status(200).json(newCharge[0]);
+	} catch (error) {
+		res.status(500).json({ message: "Ocorreu um erro interno." });
+	}
 };
 
 module.exports = postCharges;
