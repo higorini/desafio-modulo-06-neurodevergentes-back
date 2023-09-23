b6587fd6bcca728c051d714f3dc1e8b30517a43d
Francisco Walter <franciscowalterd@gmail.com>
2023-09-22 23:15:46 -0300

    finalizando a query e retornando os dados para o frontend

--- src/controllers/costumers/getCostumers.js
+++ src/controllers/costumers/getCostumers.js
@@ -4,7 +4,7 @@ const getCostumers = async (req, res) => {
 	try {
 		const { id } = req.user;
 
-		const costumersInadimplentes = await knex('costumers')
+		const defaultingCustomers = await knex('costumers')
 			.select(
 				'costumers.id as costumer_id',
 				'costumers.name as costumer_name',
@@ -21,62 +21,34 @@ const getCostumers = async (req, res) => {
 			.andWhere('charges.status', '=', 'Pendente')
 			.andWhere('costumers.user_id', '=', id);
 
-		for (const costumer of costumersInadimplentes) {
+		for (const costumer of defaultingCustomers) {
 			const costumer_id = costumer.costumer_id;
 			await knex('costumers').where('id', costumer_id).update('status', 'Inadimplente');
 		}
 
-		const costumersEmDia = await knex('costumers')
-			.select("*")
-			.whereNotIn('id', costumersInadimplentes.map(costumer => costumer.costumer_id));
+		const costumersOK = await knex('costumers')
+			.whereNotIn('id', defaultingCustomers.map(costumer => costumer.costumer_id));
 
-		for (const costumer of costumersEmDia) {
+		for (const costumer of costumersOK) {
 			const costumer_id = costumer.id;
 			await knex('costumers').where('id', costumer_id).update('status', 'Em dia');
 		}
 
 		const allCostumers = await knex("costumers").where("user_id", id);
 
+		const costumersData = allCostumers.map(({ id, user_id, name, cpf, email, phone, status }) => {
+			return {
+				id,
+				user_id,
+				name,
+				cpf,
+				email,
+				phone,
+				status
+			}
+		});
 
-		// const usersData = customers.map(
-		// 	({
-		// 		password,
-		// 		cep,
-		// 		public_place,
-		// 		complement,
-		// 		neighborhood,
-		// 		city,
-		// 		state,
-		// 		...userData
-		// 	}) => {
-		// 		const address = {
-		// 			cep,
-		// 			public_place,
-		// 			complement,
-		// 			neighborhood,
-		// 			city,
-		// 			state,
-		// 		};
-		// 		return {
-		// 			...userData,
-		// 			address,
-		// 		};
-		// 	}
-		// );
-
-		// const userData = customers.map(({ id, user_id, name, cpf, email, phone, status }) => {
-		// 	return {
-		// 		id,
-		// 		user_id,
-		// 		name,
-		// 		cpf,
-		// 		email,
-		// 		phone,
-		// 		status
-		// 	}
-		// })
-
-		return res.status(200).json(allCostumers);
+		return res.status(200).json(costumersData);
 	} catch (error) {
 		console.log(error);
 		res.status(500).json({ message: "Ocorreu um erro interno." });
