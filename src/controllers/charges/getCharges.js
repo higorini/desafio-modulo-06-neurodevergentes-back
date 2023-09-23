66b55a7a8acdacf353951dc63c48ac65b4fd28d4
Francisco Walter <franciscowalterd@gmail.com>
2023-09-23 07:07:08 -0300

    corrigindo erro na data usada como parametro para calcular o status da cobrança

--- src/controllers/charges/getCharges.js
+++ src/controllers/charges/getCharges.js
@@ -2,17 +2,18 @@ const knex = require("../../database/connection/connection");
 
 const getCharges = async (req, res) => {
   try {
-    const chargesVencidas = await knex("charges")
-    .where("charge_date", "<", knex.raw('NOW()'))
-    .where("status", "!=", "paga")
+    const defaltingCharges = await knex("charges")
+    .where("charge_date", "<", knex.raw('DATE(NOW())'))
+    .where("status", "<>", "paga")
 
-    for(const charge of chargesVencidas) {
+    for(const charge of defaltingCharges) {
       await knex("charges").update("status", "vencida").where("id", charge.id)
     };
 
-    return res.status(200).json(chargesVencidas);
+    const allCharges = await knex("charges").orderBy("status", "desc")
+
+    return res.status(200).json(allCharges);
   } catch (error) {
-    console.log(error);
     res.status(500).json({ message: "Ocorreu um erro interno." });
   }
 };
