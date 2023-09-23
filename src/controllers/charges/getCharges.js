5c38a387586f0fb436aaf07b7b5800bd27dabeb3
Francisco Walter <franciscowalterd@gmail.com>
2023-09-23 06:20:59 -0300

    implementando logica de atualizar cobranças vencidas

--- src/controllers/charges/getCharges.js
+++ src/controllers/charges/getCharges.js
@@ -2,10 +2,17 @@ const knex = require("../../database/connection/connection");
 
 const getCharges = async (req, res) => {
   try {
-    const charges = await knex("charges").orderBy("id", "asc");
+    const chargesVencidas = await knex("charges")
+    .where("charge_date", "<", knex.raw('NOW()'))
+    .where("status", "!=", "paga")
 
-    return res.status(200).json(charges);
+    for(const charge of chargesVencidas) {
+      await knex("charges").update("status", "vencida").where("id", charge.id)
+    };
+
+    return res.status(200).json(chargesVencidas);
   } catch (error) {
+    console.log(error);
     res.status(500).json({ message: "Ocorreu um erro interno." });
   }
 };
