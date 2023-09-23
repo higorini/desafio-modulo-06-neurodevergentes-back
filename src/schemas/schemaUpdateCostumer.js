1e922b3d0f915978ce700bd65e5bb8b483e29319
Teles23 <92751944+Teles23@users.noreply.github.com>
2023-09-23 00:25:04 -0300

    corrigindo validações

--- src/schemas/schemaUpdateCostumer.js
+++ src/schemas/schemaUpdateCostumer.js
@@ -43,7 +43,8 @@ const schemaUpdateCustomer = joi.object({
 	complement: joi.string().trim().empty("").allow(null).optional(),
 	neighborhood: joi.string().trim().empty("").allow(null).optional(),
 	city: joi.string().trim().empty("").allow(null).optional(),
-	state: joi.string().trim().empty("").allow(null).optional(), 
+	state: joi.string().trim().empty("").allow(null).optional(),
+	status: joi.string().allow("").optional().trim(),
 });
 
 module.exports = schemaUpdateCustomer;
