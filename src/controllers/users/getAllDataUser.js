fdf81d9d7c63fa36516cd73394972df5ef0cdaa9
Francisco Walter <franciscowalterd@gmail.com>
2023-09-24 11:37:17 -0300

    adicionado rota de envar todos os dados do cliente logado

--- src/controllers/users/getAllDataUser.js
+++ src/controllers/users/getAllDataUser.js
@@ -1,10 +1,21 @@
-const knex = require("../../database/connection/connection");
+const { getAllCharges } = require("../../utils/chargesConsults");
+const { getAllCustomers } = require("../../utils/customersConsults");
 
-const getAllDataUser = async (req, res) =>{
+const getAllDataUser = async (req, res) => {
     try {
-        
+        const { id } = req.user;
+
+        const allDatas = await Promise.all([getAllCustomers(id), getAllCharges(id)]);
+
+        const allDataUser = {
+            Customers_Data: allDatas[0],
+            Charges_Data: allDatas[1]
+        }
+
+        return res.status(200).json(allDataUser)
     } catch (error) {
-        return res.status(500).json({message: "Erro interno do servidor!"})
+        console.log(error.message);
+        return res.status(500).json({ message: "Erro interno do servidor!" })
     }
 }
 
