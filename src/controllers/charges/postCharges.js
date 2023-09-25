636878ccf7d1d8b33be6afeca79a32181f9c6f86
Thiago <92751944+Teles23@users.noreply.github.com>
2023-09-25 14:53:16 -0300

    refatorando arquivo, que trata espaços em branco

--- src/controllers/charges/postCharges.js
+++ src/controllers/charges/postCharges.js
@@ -2,36 +2,37 @@ const knex = require("../../database/connection/connection");
 const trimFields = require("../../utils/trimSpaces");
 const capitalizeFullName = require("../../utils/capitalizeName");
 
-const postCharges = async (req, res) => { 
-	const { idCostumer } = req.params;
-	let { costumer_name, description, status, charge_date, value } = req.body;
-	const { id: userId } = req.user;
+const postCharges = async (req, res) => {
+  const { idCostumer } = req.params;
+  const formattedFiels = trimFields(req.body);
+  const { id: userId } = req.user;
 
-	try {
-		const costumer = await knex("costumers")
-			.where({ id: idCostumer, user_id: userId })
-			.first();
+  try {
+    const costumer = await knex("costumers")
+      .where({ id: idCostumer, user_id: userId })
+      .first();
 
-		if (!costumer) {
-			return res.status(404).json({
-				message: "Cliente não encontrado ou não pertence ao usuário logado!",
-			});
-		}
+    if (!costumer) {
+      return res.status(404).json({
+        message: "Cliente não encontrado ou não pertence ao usuário logado!",
+      });
+    }
 
-		costumer_name = capitalizeFullName(costumer_name);
+    formattedFiels.costumer_name = capitalizeFullName(
+      formattedFiels.costumer_name
+    );
 
-		const newCharge = await knex("charges")
-			.insert({
-				costumer_id: idCostumer,
-				...trimFields({ costumer_name, description, status, charge_date }),
-				value,
-			})
-			.returning("*");
+    const newCharge = await knex("charges")
+      .insert({
+        costumer_id: idCostumer,
+        ...formattedFiels,
+      })
+      .returning("*");
 
-		return res.status(200).json(newCharge[0]);
-	} catch (error) {
-		res.status(500).json({ message: "Ocorreu um erro interno." });
-	}
+    return res.status(200).json(newCharge[0]);
+  } catch (error) {
+    res.status(500).json({ message: "Ocorreu um erro interno." });
+  }
 };
 
 module.exports = postCharges;
