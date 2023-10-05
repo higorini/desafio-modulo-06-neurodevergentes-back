69e3954b2b193d88ed8a921d08de133c79f3d8ac
Francisco Walter <franciscowalterd@gmail.com>
2023-10-05 17:20:07 -0300

    removendo console.log e renomeando variaveis

--- src/controllers/charges/searchCharge.js
+++ src/controllers/charges/searchCharge.js
@@ -9,11 +9,10 @@ const searchCharge = async (req, res) => {
             return res.status(400).json({ mensage: "Passse o Nome do cliente ou ID da cobrança na busca" })
         }
 
-        const charge = await getSearchCharge(searchCharge, id);
+        const charges = await getSearchCharge(searchCharge, id);
 
-        return res.status(200).json(charge)
+        return res.status(200).json(charges)
     } catch (error) {
-        console.log(error.message);
         return res.status(500).json({ mensage: "Erro interno do servidor" })
     }
 }
